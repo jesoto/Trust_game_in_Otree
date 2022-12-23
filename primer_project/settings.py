92e269ac5e48d408f27377da249fbaa3d315d24d
Jesús Soto <jesus.soto@pucp.edu.pe>
2022-12-22 19:08:21 -0500

    Update settings.py
    - se editó el archivo settings.py, solo se correrá de todos los experimentos sample el simple_trust
    - se elimina tambien el survey experiment
    - se edita el session config el numero de participandetes demo de 3 a 2 para que pueda existir emparejamiento

--- primer_project/settings.py
+++ primer_project/settings.py
@@ -3,13 +3,10 @@ from os import environ
 
 SESSION_CONFIGS = [
     dict(
-        name='guess_two_thirds',
-        display_name="Guess 2/3 of the Average",
-        app_sequence=['guess_two_thirds', 'payment_info'],
-        num_demo_participants=3,
-    ),
-    dict(
-        name='survey', app_sequence=['survey', 'payment_info'], num_demo_participants=1
+        name='trust_simple',
+        display_name="Behavioral Task: Trust Game",
+        app_sequence=['trust_simple', 'payment_info'],
+        num_demo_participants=2,
     ),
 ]
 
